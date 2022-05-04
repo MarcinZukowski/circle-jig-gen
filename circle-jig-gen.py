20a3902f1b01a64dd5ae0e4b511a99e92d0ecf7e
Marcin Zukowski <marcin.zukowski@gmail.com>
2022-05-04 13:35:24 -0700

    Add support for two-layers

--- circle-jig-gen.py
+++ circle-jig-gen.py
@@ -12,6 +12,10 @@ SH_WIDE = "wide"
 SH_RECTANGLE = "rectangle"
 SH_LINE = "line"
 
+LAYER_SINGLE = "single"
+LAYER_DOUBLE = "double"
+LAYER_SUPPORT = "support"
+
 CX = 80
 CY = 80
 
@@ -138,6 +142,8 @@ def main():
                         default=SCREWS_DEWALT_TRIM)
     parser.add_argument('--bigCircle', type=str, default="2.5in", help="Shape: Big circle radius")
     parser.add_argument('--smallCircle', type=str, default="1in", help="Shape: Small circle radius")
+    parser.add_argument('--layers', choices=[LAYER_SINGLE, LAYER_DOUBLE, LAYER_SUPPORT],
+                        help="How to arrange layers", default=LAYER_SINGLE)
 
     args = parser.parse_args()
     inches = args.inches
@@ -192,155 +198,199 @@ def main():
     screws = args.screws
     bigCircleRadius = unit(args.bigCircle)
     smallCircleRadius = unit(args.smallCircle)
+    layers = args.layers
 
     d = Drawer()
 
+    # Print command line
     for idx, arg in enumerate(sys.argv):
         d.text(3, 3 + idx * 3, arg, fs=3, anchor="start", color=d.MARK)
-
-    # Hole for the bit
-    d.circle(CX, CY, bitDiam / 2, d.CUT)
-
-    # Hole for the cut
-    d.circle(CX, CY, cutRadius, d.CUT)
-
-    # Screw holes
-    screw_holes = screws.split(";")
-    for screw_hole in screw_holes:
-        attrs = screw_hole.split(",")
-        assert len(attrs) in [3, 4]
-        sx = CX + unit(attrs[0])
-        sy = CY + unit(attrs[1])
-        sr = unit(attrs[2]) / 2
-        d.circle(sx, sy, sr, d.CUT)
-        d.cross(sx, sy, 1, d.MARK)
-        if len(attrs) == 4:
-            sr = unit(attrs[3]) / 2
-            d.circle(sx, sy, sr, d.MARK)
 
     def compRadius(step, subStep):
         return bitRadius + minRadius + step * stepSize + subStep * stepSize / subSteps
 
-    def pinHolePosition(step, subStep):
-        nonlocal x, y
-        ang = subStep * stepAngle - (subSteps - 1) * stepAngle / 2
-        angrad = math.radians(ang)
-        radius = compRadius(step, subStep)
-        if subSteps == 1 or shape == SH_LINE:
-            x = radius
-            y = 0
-        elif shape == SH_RECTANGLE:
-            # Draw holes in a rectangle
-            substepWidth = 8
-            y = (subSteps - 1) * substepWidth / 2 - subStep * substepWidth
-            x = math.sqrt(radius * radius - y * y)
-        elif shape == SH_WIDE:
-            # Draw holes expanding from the bit
-            x = math.cos(angrad) * radius
-            y = - math.sin(angrad) * radius
-        elif shape == SH_NARROW:
-            # Draw holes expanding from far away from the bit
-            # Math based on
-            # https://www.calculator.net/triangle-calculator.html?vc=&vx=20&vy=&va=&vz=60&vb=7&angleunits=d&x=0&y=0
-            far = minRadius + (steps - 1) * stepSize * 2
-            B = angrad
-            c = far
-            b = radius
-            C = math.pi - math.asin(c * math.sin(B) / b)
-            A = math.pi - B - C
-            a = b * math.sin(A) / math.sin(B)
-            x = far - math.cos(angrad) * a
-            y = - math.sin(angrad) * a
-        else:
-            assert False, f"Unknown shape: {shape}"
-        return CX+x, CY+y
+    def generatePins(cx, cy, bottom=False):
 
-    # Holes for the pins
-    for step in range(0, steps):
-        for subStep in range(0, subSteps):
-            x, y = pinHolePosition(step, subStep)
-            d.circle(x, y, pinRadius, d.CUT)
+        def pinHolePosition(step, subStep):
+            nonlocal x, y
+            ang = subStep * stepAngle - (subSteps - 1) * stepAngle / 2
+            angrad = math.radians(ang)
+            radius = compRadius(step, subStep)
+            if subSteps == 1 or shape == SH_LINE:
+                x = radius
+                y = 0
+            elif shape == SH_RECTANGLE:
+                # Draw holes in a rectangle
+                substepWidth = 8
+                y = (subSteps - 1) * substepWidth / 2 - subStep * substepWidth
+                x = math.sqrt(radius * radius - y * y)
+            elif shape == SH_WIDE:
+                # Draw holes expanding from the bit
+                x = math.cos(angrad) * radius
+                y = - math.sin(angrad) * radius
+            elif shape == SH_NARROW:
+                # Draw holes expanding from far away from the bit
+                # Math based on
+                # https://www.calculator.net/triangle-calculator.html?vc=&vx=20&vy=&va=&vz=60&vb=7&angleunits=d&x=0&y=0
+                far = minRadius + (steps - 1) * stepSize * 2
+                B = angrad
+                c = far
+                b = radius
+                C = math.pi - math.asin(c * math.sin(B) / b)
+                A = math.pi - B - C
+                a = b * math.sin(A) / math.sin(B)
+                x = far - math.cos(angrad) * a
+                y = - math.sin(angrad) * a
+            else:
+                assert False, f"Unknown shape: {shape}"
+            return cx + x, cy + y
 
-    # Per-substep/angle guides
-    if shape != SH_LINE:
-        for subStep in range(0, subSteps):
-            x0, y0 = pinHolePosition(0, subStep)
-            x1, y1 = pinHolePosition(steps - 1, subStep)
-            d.line(x0, y0, x1, y1, d.GREEN, 'stroke-dasharray="10,10" stroke-width="0.5"')
-
-    # Per-step/major guides
-    if shape != SH_LINE:
+        # Holes for the pins
         for step in range(0, steps):
-            for subStep in range(1, subSteps):
-                x0, y0 = pinHolePosition(step, subStep - 1)
-                x1, y1 = pinHolePosition(step, subStep)
-                d.line(x0, y0, x1, y1, d.GREEN, 'stroke-dasharray="5, 5" stroke-width="0.25"')
-
-    # Per-step labels
-    for step in range(0, steps):
-        s = unitStr(minRadius + step * stepSize)
-        x, y = pinHolePosition(step, 0)
-        d.text(0, 0, s, anchor="end", fs=6, color=d.MARK, extra=f'transform="translate({(x + 2) *3.7795}, {(y + 5)*MM2PX}) rotate(270)"')
-        d.line(x, y + 1, x, y + 4, d.MARK)
-        if shape != SH_LINE:
-            s = unitStr(minRadius + step * stepSize + (subSteps - 1) * stepSize / subSteps)
-            x, y = pinHolePosition(step, subSteps - 1)
-            d.text(0, 0, s, anchor="start", fs=4, color=d.MARK, extra=f'transform="translate({(x + 2) *3.7795}, {(y - 5)*MM2PX}) rotate(270)"')
-            d.line(x, y - 1, x, y - 4, d.MARK)
-
-    if shape != SH_LINE:
-        # Per-angle labels
-        if subSteps > 0:
-            for subStep in range(1, subSteps):
-                s = "+" + unitStr(subStep * stepSize / subSteps)
-                x, y = pinHolePosition(0, subStep)
-                d.text(x - 2, y + 1, s, anchor="end", fs=4, color=d.MARK)
-                x, y = pinHolePosition(steps - 1, subStep)
-                d.text(x + 2, y + 1, s, anchor="start", fs=4, color=d.MARK)
-    else:
-        # Minor tick albels
-        for step in range(0, steps):
-            for subStep in range(1, subSteps):
+            for subStep in range(0, subSteps):
                 x, y = pinHolePosition(step, subStep)
-                s = "+" + unitStr(subStep * stepSize / subSteps)
-                d.text(0, 0, s, anchor="start", fs=3, color=d.MARK,
-                       extra=f'transform="translate({(x + 1) * 3.7795}, {(y - 2) * 3.7795}) rotate(270)"')
+                d.circle(x, y, pinRadius, d.CUT)
 
-    # draw shape around
-    bcr = bigCircleRadius
-    scr = smallCircleRadius
-    scd = compRadius(steps, 0)    # small circle distance
+        if not bottom:
+            # Per-substep/angle guides
+            if shape != SH_LINE:
+                for subStep in range(0, subSteps):
+                    x0, y0 = pinHolePosition(0, subStep)
+                    x1, y1 = pinHolePosition(steps - 1, subStep)
+                    d.line(x0, y0, x1, y1, d.GREEN, 'stroke-dasharray="10,10" stroke-width="0.5"')
 
-    if shape != SH_RECTANGLE:
-        # Angle for the line adjactent to two circles
-        rd = bcr - scr  # radius difference triangle
-        ang = math.acos(rd / scd)
-        dbg(ang)
+            # Per-step/major guides
+            if shape != SH_LINE:
+                for step in range(0, steps):
+                    for subStep in range(1, subSteps):
+                        x0, y0 = pinHolePosition(step, subStep - 1)
+                        x1, y1 = pinHolePosition(step, subStep)
+                        d.line(x0, y0, x1, y1, d.GREEN, 'stroke-dasharray="5, 5" stroke-width="0.25"')
 
-        # Left arc
-        bx = math.cos(ang) * bcr
-        by = math.sin(ang) * bcr
-        d.circle(CX + bx, CY + by, 1, color=d.DBG)
-        d.circle(CX + bx, CY - by, 1, color=d.DBG)
-        d.arc(CX, CY, bcr, bx, by, color=d.CUT)
+            # Per-step labels
+            for step in range(0, steps):
+                s = unitStr(minRadius + step * stepSize)
+                x, y = pinHolePosition(step, 0)
+                d.text(0, 0, s, anchor="end", fs=6, color=d.MARK, extra=f'transform="translate({(x + 2) *3.7795}, {(y + 5)*MM2PX}) rotate(270)"')
+                d.line(x, y + 1, x, y + 4, d.MARK)
+                if shape != SH_LINE:
+                    s = unitStr(minRadius + step * stepSize + (subSteps - 1) * stepSize / subSteps)
+                    x, y = pinHolePosition(step, subSteps - 1)
+                    d.text(0, 0, s, anchor="start", fs=4, color=d.MARK, extra=f'transform="translate({(x + 2) *3.7795}, {(y - 5)*MM2PX}) rotate(270)"')
+                    d.line(x, y - 1, x, y - 4, d.MARK)
 
-        # Right arc
-        sx = math.cos(ang) * scr
-        sy = math.sin(ang) * scr
-        d.circle(CX + scd + sx, CY + sy, 1, color=d.DBG)
-        d.circle(CX + scd + sx, CY - sy, 1, color=d.DBG)
-        d.arc(CX + scd, CY, scr, sx, sy, color=d.CUT, reverse=1)
+            if shape != SH_LINE:
+                # Per-angle labels
+                if subSteps > 0:
+                    for subStep in range(1, subSteps):
+                        s = "+" + unitStr(subStep * stepSize / subSteps)
+                        x, y = pinHolePosition(0, subStep)
+                        d.text(x - 2, y + 1, s, anchor="end", fs=4, color=d.MARK)
+                        x, y = pinHolePosition(steps - 1, subStep)
+                        d.text(x + 2, y + 1, s, anchor="start", fs=4, color=d.MARK)
+            else:
+                # Minor tick labels
+                for step in range(0, steps):
+                    for subStep in range(1, subSteps):
+                        x, y = pinHolePosition(step, subStep)
+                        s = "+" + unitStr(subStep * stepSize / subSteps)
+                        d.text(0, 0, s, anchor="start", fs=3, color=d.MARK,
+                               extra=f'transform="translate({(x + 1) * 3.7795}, {(y - 2) * 3.7795}) rotate(270)"')
 
-        # Connecting lines
-        d.line(CX + bx, CY + by, CX + scd + sx, CY + sy, color=d.CUT)
-        d.line(CX + bx, CY - by, CX + scd + sx, CY - sy, color=d.CUT)
+    def outline(cx, cy):
+        # draw shape around
+        bcr = bigCircleRadius
+        scr = smallCircleRadius
+        scd = compRadius(steps, 0)    # small circle distance
 
-    else:
-        # "Rectangle"
-        d.arc(CX, CY, bcr, 0, bcr, color=d.CUT)
-        d.line(CX, CY + bcr, CX + scd + scr, CY + bcr, color=d.CUT)
-        d.line(CX, CY - bcr, CX + scd + scr, CY - bcr, color=d.CUT)
-        d.line(CX + scd + scr, CY - bcr, CX + scd + scr, CY + bcr, color=d.CUT)
+        if shape != SH_RECTANGLE:
+            # Angle for the line adjactent to two circles
+            rd = bcr - scr  # radius difference triangle
+            ang = math.acos(rd / scd)
+            dbg(ang)
+
+            # Left arc
+            bx = math.cos(ang) * bcr
+            by = math.sin(ang) * bcr
+            d.circle(cx + bx, cy + by, 1, color=d.DBG)
+            d.circle(cx + bx, cy - by, 1, color=d.DBG)
+            d.arc(cx, cy, bcr, bx, by, color=d.CUT)
+
+            # Right arc
+            sx = math.cos(ang) * scr
+            sy = math.sin(ang) * scr
+            d.circle(cx + scd + sx, cy + sy, 1, color=d.DBG)
+            d.circle(cx + scd + sx, cy - sy, 1, color=d.DBG)
+            d.arc(cx + scd, cy, scr, sx, sy, color=d.CUT, reverse=1)
+
+            # Connecting lines
+            d.line(cx + bx, cy + by, cx + scd + sx, CY + sy, color=d.CUT)
+            d.line(cx + bx, cy - by, cx + scd + sx, CY - sy, color=d.CUT)
+
+        else:
+            # "Rectangle"
+            d.arc(cx, cy, bcr, 0, bcr, color=d.CUT)
+            d.line(cx, cy + bcr, cx + scd + scr, cy + bcr, color=d.CUT)
+            d.line(cx, cy - bcr, cx + scd + scr, cy - bcr, color=d.CUT)
+            d.line(cx + scd + scr, cy - bcr, cx + scd + scr, cy + bcr, color=d.CUT)
+
+    def support(cx, cy):
+        # draw shape around the router base
+        d.circle(cx, cy, bigCircleRadius, color=d.CUT)
+        # draw a supporting piece
+        x2 = cx + bigCircleRadius + 10 + smallCircleRadius
+        y2 = cy - bigCircleRadius + smallCircleRadius
+        d.circle(x2, y2, pinRadius, color=d.CUT)
+        d.cross(x2, y2, smallCircleRadius, color=d.MARK)
+        d.circle(x2, y2, smallCircleRadius, color=d.CUT)
+
+    def routerBase(cx, cy, bottom):
+        # Hole for the bit
+        d.circle(cx, cy, bitDiam / 2, d.CUT)
+
+        # Hole for the cut
+        d.circle(cx, cy, cutRadius, d.CUT)
+
+        # Screw holes
+        screw_holes = screws.split(";")
+        for screw_hole in screw_holes:
+            attrs = screw_hole.split(",")
+            assert len(attrs) in [3, 4]
+            sx = cx + unit(attrs[0])
+            sy = cy + unit(attrs[1])
+            sr = unit(attrs[2]) / 2
+            d.cross(sx, sy, 1, d.MARK)
+            if not bottom or len(attrs) == 3:
+                # smaller hole
+                d.circle(sx, sy, sr, d.CUT)
+                if layers == LAYER_SINGLE and len(attrs) == 4:
+                    sr = unit(attrs[3]) / 2
+                    d.circle(sx, sy, sr, d.MARK)
+            elif bottom:
+                if len(attrs) == 4:
+                    sr = unit(attrs[3]) / 2
+                d.circle(sx, sy, sr, d.CUT)
+
+    def glueGuides(cx, cy):
+        d.circle(cx - bigCircleRadius + 5, cy, pinRadius, color=d.CUT)
+        d.circle(cx, cy - bigCircleRadius + 5, pinRadius, color=d.CUT)
+        d.circle(cx, cy + bigCircleRadius - 5, pinRadius, color=d.CUT)
+
+    # First layer
+    routerBase(CX, CY, bottom=False)
+    generatePins(CX, CY, bottom=False)
+    outline(CX, CY)
+    # Second layer, if needed
+    CY2 = CY + 2 * bigCircleRadius + 20
+    if layers in [LAYER_DOUBLE, LAYER_SUPPORT]:
+        routerBase(CX, CY2, bottom=True)
+        glueGuides(CX, CY)
+        glueGuides(CX, CY2)
+        if layers == LAYER_DOUBLE:
+            generatePins(CX, CY2, bottom=True)
+            outline(CX, CY2)
+        else:
+            support(CX, CY2)
 
     print(d.write())
 
